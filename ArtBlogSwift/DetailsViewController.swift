f2fd103491227ceb6ffdfbd1072039273c4ae308
mehmetfurkansakiz <mehmetfurkansakiz@gmail.com>
2023-07-29 02:27:11 +0300

    Design UI with text fields, segue and image views in main.storyboard

--- ArtBlogSwift/DetailsViewController.swift
+++ ArtBlogSwift/DetailsViewController.swift
@@ -9,21 +9,21 @@ import UIKit
 
 class DetailsViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var artistText: UITextField!
+    @IBOutlet weak var yearText: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+  
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func saveButtonClicked(_ sender: Any) {
+        
     }
-    */
+    
 
 }
